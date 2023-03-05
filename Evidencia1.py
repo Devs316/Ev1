24465afa4ed84e5d10ed5cd9c314cccd88d91ea1
Devs316 <127011459+Devs316@users.noreply.github.com>
2023-03-05 07:13:45 -0600

    ik

--- Evidencia1.py
+++ Evidencia1.py
@@ -12,23 +12,24 @@ opción = int(input("Seleccione el número de la acción que desea realizar \n:"
 
 if opción == 1:
     print('******** REGISTRO DE NUEVO LIBRO ********')
-    titulo = input('Ingrese el nombre de la obra: ')
-    autor = input('Ingrese el autor de la obra: ')
-    genero = input('Ingrese el genero de la obra: ')
-    año_publicacion = input("Ingrese el año de publicacion: ")
+    titulo = input('Ingrese el nombre de la obra: ').upper()
+    autor = input('Ingrese el autor de la obra: ').upper()
+    genero = input('Ingrese el genero de la obra: ').upper()
+    año_publicacion = input("Ingrese el año de publicacion: ").upper()
     año_publicacion = datetime.datatime.striptime(año_publicacion,"%Y").date()   
-    isbn=input("Ingrese el ISBN de la obra: ") 
-    fecha_adquisicion = input("Ingrese la fecha de su evento  (dd/mm/aaaa): ")
+    isbn=input("Ingrese el ISBN de la obra: ").upper
+    fecha_adquisicion = input("Ingrese la fecha de su evento  (dd/mm/aaaa): ").upper
     fecha_adquisicion = datetime.datatime.striptime(fecha_adquisicion,"%d/%m/%Y").date()
     
 
-if opción == 2:
-    while True:
-        submenu_opcion = int(input("Seleccione el numero de la opcion que quiere elegir \n:" ))
+elif opción == 2:
+    while True:   
         print("1. Consulta de titulo")
         print("2. Reportes")
         print("3. Volver al menu de consultas y reportes")
-        
+    submenu_opcion = int(input("Seleccione el numero de la opcion que quiere elegir \n:" ))
+    
+
 else:
     print("ADIOS,GRACIAS POR VISITAR EL CATALOGO DE LIBROS")
 exit()
