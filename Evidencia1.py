33af20a0cabb8a9f83d35ea9c41749e3a27792b6
Migueltak <127013101+Migueltak@users.noreply.github.com>
2023-03-05 03:42:51 -0600

    Update Evidencia1.py

--- Evidencia1.py
+++ Evidencia1.py
@@ -2,8 +2,8 @@ print("*" *10 + " BIENVENIDO " + "*" *10)
 
 while true:
 print("1, Registrar una reserva")
-print("2
-print(
-print(
-print(
-print(
+print("2, Editar un evento ya existente")
+print("3, Consulta tus reservas")
+print("4, Registrarte cómo nuevo cliente")
+print("5, Registrar una Sala")
+print("6, Salir")
