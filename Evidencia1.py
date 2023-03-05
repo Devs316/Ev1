197602513ebc422e05fd8beb5e93d73c07874e80
Migueltak <127013101+Migueltak@users.noreply.github.com>
2023-03-05 04:24:36 -0600

    Update Evidencia1.py

--- Evidencia1.py
+++ Evidencia1.py
@@ -11,5 +11,13 @@ if opción == 1:
     print('******** REGISTRO PARA RESERVACION DE UNA SALA ********')
     nombre_evento = input('Ingrese el nombre de su evento: ')
     fecha_reservada = input("Ingrese la fecha de su evento  (dd/mm/aaaa): ")
-fecha_reservada = datetime.datatime.striptime(fecha_reseevada,"%d/%m/%Y").date()
+    fecha_reservada = datetime.datatime.striptime(fecha_reseevada,"%d/%m/%Y").date()
 
+    día_reservado = fecha_reservada.day
+    mes_reservado = fecha_reservada.month
+    año_ressevado = fecha_reservada.year
+
+    fecha_actual = datetime.date.today()
+    dia_actual = fecha_actual.day
+    mes_actual = fecha_actual.month
+    año_reseevado = fecha_actual.year
